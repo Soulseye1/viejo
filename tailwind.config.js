960fec212db25be679da9d222c31179296860f73
Soulseye1 <orlando.diaz@icloud.com>
2025-04-08 12:35:09 -0600

    tail new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
